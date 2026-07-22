1852438fb8b1e8f05770f9646c56609a0e165e11
matusmatus509-cmyk <matusmatus509-cmyk@users.noreply.github.com>
2026-07-22 14:50:33 +0200

    Expand charades game library

--- src/data/charades.ts
+++ src/data/charades.ts
@@ -60,33 +60,39 @@ export const SOLO_CHARADES_WORDS: Record<CharadesDifficulty, string[]> = {
   ]),
 };
 
-// Tímová hra používa druhý, samostatný balík. Zámerne nie je vytvorený zo
-// SOLO_CHARADES_WORDS, aby obe hry mali nezávislé zásoby a poradie.
+// Tímová hra používa vlastné nové polia a vlastné miešanie. Univerzálny rozšírený
+// katalóg sa pri vytvorení skopíruje, takže zmeny balíka počas hry neovplyvnia sólo hru.
 export const TEAM_CHARADES_WORDS: Record<CharadesDifficulty, string[]> = {
-  lahke: [
+  lahke: uniqueWords([
     "Lev", "Tiger", "Opica", "Zebra", "Krokodíl", "Žaba", "Králik", "Veverička", "Medveď", "Sova",
     "Hruška", "Pomaranč", "Chlieb", "Syr", "Polievka", "Ryža", "Koláč", "Klobása", "Ananás", "Uhorka",
     "Stôl", "Skriňa", "Televízor", "Chladnička", "Lyžica", "Vidlička", "Kefa", "Uterák", "Lampa", "Kniha",
     "Električka", "Metro", "Taxík", "Rakieta", "Ponorka", "Karavan", "Nákladné auto", "Výťah", "Kočík", "Trajekt",
     "Beh", "Cyklistika", "Šach", "Bowling", "Šípky", "Surfovanie", "Skok do výšky", "Stolný tenis", "Gymnastika", "Rybárčenie",
     "Herec", "Spevák", "Farmár", "Poštár", "Sudca", "Čašník", "Predavač", "Veterinár", "Programátor", "Stavbár",
-  ],
-  stredne: [
+    ...SOLO_CHARADES_WORDS.lahke,
+    ...SOLO_CHARADES_EASY_EXTRA,
+  ]),
+  stredne: uniqueWords([
     "Umývačka riadu", "Elektrická kolobežka", "Bezdrôtová myš", "Herná konzola", "Inteligentné hodinky", "Solárny panel", "Požiarny hlásič", "Nákupný vozík", "Kancelárska tlačiareň", "Robotický vysávač",
     "Autobusová zastávka", "Diaľničný tunel", "Podzemná garáž", "Prístavný maják", "Lyžiarske stredisko", "Mestská knižnica", "Nákupné centrum", "Futbalový štadión", "Botanická záhrada", "Zábavný park",
     "Promócie", "Stužková slávnosť", "Rozlúčka so slobodou", "Detská besiedka", "Maturitná skúška", "Pracovný pohovor", "Triedne stretnutie", "Nedeľný obed", "Letná dovolenka", "Silvestrovská párty",
     "Výmena pneumatiky", "Stavanie stanu", "Kosenie trávnika", "Maľovanie steny", "Skladanie nábytku", "Hľadanie signálu", "Rezervácia hotela", "Kontrola lístka", "Fotografovanie západu slnka", "Rozbaľovanie balíka",
     "Meteorológ", "Geológ", "Architekt", "Tlmočník", "Novinár", "Režisér", "Knihovník", "Laborant", "Dispečer", "Tréner",
     "Pirátska loď", "Ľadový palác", "Podzemné mesto", "Tropická búrka", "Zabudnutá civilizácia", "Tajné laboratórium", "Časostroj", "Lietajúci koberec", "Zakliaty hrad", "Mimozemská planéta",
-  ],
-  tazke: [
+    ...SOLO_CHARADES_WORDS.stredne,
+    ...SOLO_CHARADES_MEDIUM_EXTRA,
+  ]),
+  tazke: uniqueWords([
     "Svedomie", "Predsudok", "Sebavedomie", "Nostalgia", "Solidarita", "Pokrytectvo", "Tolerancia", "Intuícia", "Ambícia", "Disciplína",
     "Bod zlomu", "Za päť minút dvanásť", "Mať maslo na hlave", "Prilievať olej do ohňa", "Nosiť drevo do lesa", "Robiť z komára somára", "Vziať nohy na plecia", "Trafiť klinec po hlavičke", "Mať oči na stopkách", "Ostať na ocot",
     "Kvantová fyzika", "Čierna diera", "Fotosyntéza", "Tektonická doska", "Prirodzený výber", "Elektromagnetické pole", "Kozmické žiarenie", "Reťazová reakcia", "Biologická rovnováha", "Digitálna stopa",
     "Mierová konferencia", "Ústavný zákon", "Priemyselná revolúcia", "Humanitárna pomoc", "Menová reforma", "Občianska neposlušnosť", "Sloboda prejavu", "Ochrana súkromia", "Konflikt záujmov", "Prezumpcia neviny",
     "Stratená pamäť", "Dokonalý podvod", "Utajená identita", "Posledný svedok", "Nemožná misia", "Nevinný podozrivý", "Záhadný odkaz", "Nečakaný spojenec", "Tajný vchod", "Falošné obvinenie",
     "Zakázané ovocie", "Jablko sváru", "Damoklov meč", "Sizyfovská práca", "Trójsky kôň", "Šalamúnske rozhodnutie", "Kolumbovo vajce", "Gordický uzol", "Ariadnina niť", "Prokrustovo lôžko",
-  ],
+    ...SOLO_CHARADES_WORDS.tazke,
+    ...SOLO_CHARADES_HARD_EXTRA,
+  ]),
 };
 
 export const ALL_SOLO_CHARADES_WORDS = uniqueWords(Object.values(SOLO_CHARADES_WORDS).flat());
